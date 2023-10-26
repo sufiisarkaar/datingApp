a6141907d07d3a24b665479bbf7b16fe6a34a09a
rizwan8880 <rizwanansari8880@gmail.com>
2023-10-26 17:27:35 +0530

    login/signup with validation work done

--- datingAppFront/src/app/components/login/login.component.ts
+++ datingAppFront/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  loginForm :FormGroup
+  showPassword: boolean = false; 
   constructor() { }
 
   ngOnInit(): void {
+     //loginForm
+     this.loginForm = new FormGroup({
+     
+      'loginEmail':new FormControl( '',Validators.compose([Validators.required,Validators.email])),
+      'loginPassword' :new FormControl('',Validators.required),
+     
+    })
   }
+  loginData(){
+    console.log("loginData====>",this.loginForm.value);
+    }
+    get loginControls(){
+      return this.loginForm.controls
+    }
+    togglePasswordVisibility(){
 
+  this.showPassword =!this.showPassword
+}
 }
--- datingAppFront/src/app/components/register/register.component.ts
+++ datingAppFront/src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators,  } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators, } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,60 +7,61 @@ import { FormBuilder, FormControl, FormGroup, Validators,  } from '@angular/form
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-    SignupForm : FormGroup;
-    loginForm :FormGroup
-    submitted : boolean
+  SignupForm: FormGroup;
+  showconPassword: boolean = false
+  showPassword: boolean = false;
+  submitted: boolean
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     //singnupForm
     this.SignupForm = new FormGroup({
-      'firstName' :new FormControl('',Validators.compose([Validators.required,Validators.minLength(4),]),),
-      'lastName' :new FormControl('',Validators.required),
-      'email':new FormControl( '',Validators.compose([Validators.required,Validators.email])),
-      'password' :new FormControl('',Validators.required),
-      'confirmPassword' :new FormControl('',Validators.required)
+      'firstName': new FormControl('', Validators.compose([Validators.required, Validators.minLength(4),]),),
+      'lastName': new FormControl('', Validators.required),
+      'email': new FormControl('', Validators.compose([Validators.required, Validators.email])),
+      'password': new FormControl('', Validators.required),
+      'confirmPassword': new FormControl('', Validators.required)
     },
-    { 
-      validators :<any> this.mustMatch('password','confirmPassword')
-    }
+      {
+        validators: <any>this.mustMatch('password', 'confirmPassword')
+      }
     )
 
-    //loginForm
-    this.loginForm = new FormGroup({
-     
-      'loginEmail':new FormControl( '',Validators.compose([Validators.required,Validators.email])),
-      'loginPassword' :new FormControl('',Validators.required),
-     
-    })
+
   }
-signupData(){
-  console.log("signupData",this.SignupForm.value);
-  
-}
-loginData(){
-  console.log("loginData====>",this.loginForm.value);
-  }
-  get f(){
-    return this.SignupForm.controls
-  }
-  get loginControls(){
-    return this.loginForm.controls
+  signupData() {
+    console.log("signupData", this.SignupForm.value);
+
   }
 
-  mustMatch(password:any ,conpassword :any){
-    return (FormGroup:FormGroup)=>{
-      const passwordControl=FormGroup.controls[password]
-      const conpasswordControl=FormGroup.controls[conpassword]
-      if(conpasswordControl.errors &&!conpasswordControl.errors['mustMatch']){
+  get f() {
+    return this.SignupForm.controls
+  }
+
+
+  mustMatch(password: any, conpassword: any) {
+    return (FormGroup: FormGroup) => {
+      const passwordControl = FormGroup.controls[password]
+      const conpasswordControl = FormGroup.controls[conpassword]
+      if (conpasswordControl.errors && !conpasswordControl.errors['mustMatch']) {
         return
       }
-      if(passwordControl.value!==conpasswordControl.value){
-        conpasswordControl.setErrors({mustMatch:true})
-              }
-              else{
-                conpasswordControl.setErrors(null)
-              }
+      if (passwordControl.value !== conpasswordControl.value) {
+        conpasswordControl.setErrors({ mustMatch: true })
+      }
+      else {
+        conpasswordControl.setErrors(null)
+      }
+    }
+  }
+  togglePasswordVisibility(e): void {
+    if (e == "confirmPassword") {
+
+      this.showconPassword = !this.showconPassword
+    }
+    else {
+
+      this.showPassword = !this.showPassword; //  Toggle the password visibility
     }
   }
 }
