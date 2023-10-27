0e46789866f42835dd591206ffe2aa7fd8bf10da
sufiisarkaar <sufiisarkaar@gmail.com>
2023-10-27 17:33:50 +0530

    final commit

--- datingAppFront/src/app/app-routing.module.ts
+++ datingAppFront/src/app/app-routing.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
+import { PrivacyComponent } from './components/privacy/privacy.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ServiceComponent } from './components/service/service.component';
 
 const routes: Routes = [  
   {path: '', component:LandingComponent},
   { path: 'login', component: LoginComponent },
   {path: 'register', component:RegisterComponent},
+  {path: 'service', component:ServiceComponent},
+  {path: 'blog', component:BlogComponent},
+  {path: 'privacy', component:PrivacyComponent},
+  {path: 'contact', component:ContactComponent},
+  {path: 'about', component:AboutComponent},
 ];
 
 @NgModule({
